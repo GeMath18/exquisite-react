eaf4c8073c4ab20ab90ecd2bda252a3da08f01eb
gessica <gessicamohr18@gmail.com>
2021-01-05 22:16:30 -0800

    created event handles for playersubmissionform

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -3,21 +3,90 @@ import PropTypes from 'prop-types';
 
 import './PlayerSubmissionForm.css';
 
-const PlayerSubmissionForm = () => {
+const PlayerSubmissionForm = (props) => {
+
+  const [formFields, setFormFields] = useState({
+    adj1: '',
+    noun1: '',
+    adverb: '',
+    verb: '',
+    adj2: '',
+    noun2: '',
+  });
+  
+  const onInputChange = (event) => {
+    const newFormFields = {
+      ...formFields,
+    }
+
+    newFormFields[event.target.name] = event.target.value;
+    setFormFields(newFormFields);
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    setFormFields({
+      adj1: '',
+      noun1: '',
+      adverb: '',
+      verb: '',
+      adj2: '',
+      noun2: '',
+
+    });
+  }
+  
+  
   return (
     <div className="PlayerSubmissionForm">
-      <h3>Player Submission Form for Player #{  }</h3>
+      <h3>Player Submission Form for Player #{props.index}</h3>
 
-      <form className="PlayerSubmissionForm__form" >
+      <form className="PlayerSubmissionForm__form" onSubmit={onFormSubmit}>
 
         <div className="PlayerSubmissionForm__poem-inputs">
-
-          {
-            // Put your form inputs here... We've put in one below as an example
-          }
+          The
           <input
-            placeholder="hm..."
-            type="text" />
+            name="adj1"
+            placeholder="adjective1"
+            type="text"
+            value={formFields.adj1}
+            onChange={onInputChange} />
+
+          <input
+            name="noun1"
+            placeholder="noun1"
+            type="text"
+            value={formFields.noun1}
+            onChange={onInputChange} />
+
+          <input
+            name="adverb"
+            placeholder="adverb"
+            type="text"
+            value={formFields.adverb}
+            onChange={onInputChange} />
+
+          <input
+            name="verb"
+            placeholder="verb"
+            type="text"
+            value={formFields.verb}
+            onChange={onInputChange} />
+
+          <input 
+            placeholder="adjective2"
+            name="adj2"
+            type="text"
+            value={formFields.adj2}
+            onChange={onInputChange} />
+
+          <input 
+            placeholder="noun2"
+            name="noun2"
+            type="text"
+            value={formFields.noun2}
+            onChange={onInputChange} />
 
         </div>
 
