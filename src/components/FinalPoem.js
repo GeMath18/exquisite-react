7a830daef95b910a7527ec86a3666749a8b48ebe
gessica <gessicamohr18@gmail.com>
2021-01-06 12:27:16 -0800

    tests for finalpoem passing, still working on Wave 2

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -3,20 +3,30 @@ import PropTypes from 'prop-types';
 import './FinalPoem.css';
 
 const FinalPoem = (props) => {
+  if (props.isSubmitted) {
+    return (
+      <div className="FinalPoem">
+        <section className="FinalPoem__poem">
+          <h3>Final Poem</h3>
+          {props.submissions.map((submission, i) =>(
+            <p key={i}>{submission}</p>
+          ))
+        }
+        </section>
+        </div>
+        )
+      } else {
+        return (
+        <div className="FinalPoem">
+          <div className="FinalPoem__reveal-btn-container">
+            <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" onClick={props.revealPoem}/>
+          </div>
+        </div>
+        );
+      }
+};
+     
 
-  return (
-    <div className="FinalPoem">
-      <section className="FinalPoem__poem">
-        <h3>Final Poem</h3>
-
-      </section>
-
-      <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
-      </div>
-    </div>
-  );
-}
 
 FinalPoem.propTypes = {
   isSubmitted: PropTypes.bool.isRequired,
